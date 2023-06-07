5ad7efde33b2fdee6636819dcf7f8b9c71a8bbeb
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2023-06-07 15:15:15 +0530

    seeder file change

--- backend/seeder.js
+++ backend/seeder.js
@@ -3,31 +3,37 @@ const TrainSeat = require('./model/trainSeat')
 
 const seeder = async()=>{
 
-    // await TrainSeat.deleteMany()
+    await TrainSeat.deleteMany()
 
-    const data = await TrainSeat.find({});
-
-    if(data.length!==0){
-        await TrainSeat.updateMany({booked:false})
-    }
-    else{
         let seats=[]
         for (let i = 1; i <= 80; i++) {
-            if(i%6==0){
-                seats.push({
-                    seatNumber: i,
-                    booked: true,
-                    });
-            }else{
-                seats.push({
-                    seatNumber: i,
-                    booked: false,
-                    });
+            seats.push({
+                seatNumber: i,
+                booked: false,
+                });
+        }
+            
+
+        const randomNumbers = [];
+
+        for (let i = 0; i < 10; i++) {
+            // Generate a random number between 1 and 80
+            const randomNumber = Math.floor(Math.random() * 80) + 1;
+
+            // Check if the random number is already in the array
+            if (!randomNumbers.includes(randomNumber)) {
+                // Push the random number into the array
+                randomNumbers.push(randomNumber);
             }
         }
-    
+
+        for(let nums of randomNumbers){
+            seats[nums].booked=true;
+        }
+
+            
         const res= await TrainSeat.insertMany(seats);
-    }
+    
    
 }
 
